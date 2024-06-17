65616fa8ccf3b8237775e0047f99670dae423bdc
ridwaanhall <erbyl14@gmail.com>
2024-06-17 20:53:46 +0700

    add percentages

--- base/model.py
+++ base/model.py
@@ -1,22 +1,27 @@
-import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
+import tensorflow as tf
 
-# Load your model
 model = tf.keras.models.load_model('zeronine.h5')
+class_labels = ['eight', 'five', 'four', 'nine', 'one', 'seven', 'six', 'three', 'two', 'zero']
 
-# Define a function to preprocess and predict
-def predict_image(img_path):
-    img = image.load_img(img_path, target_size=(150, 150))
+def predict(img):
+    img = img.resize((150, 150))
+
     img_tensor = image.img_to_array(img)
+
     img_tensor = np.expand_dims(img_tensor, axis=0)
+
     img_tensor /= 255.
 
     prediction = model.predict(img_tensor)
-    prediction_probabilities = prediction[0]
-    sorted_indices = np.argsort(prediction_probabilities)[::-1]
 
-    labels = ['eight', 'five', 'four', 'nine', 'one', 'seven', 'six', 'three', 'two', 'zero']
-    sorted_labels = [(labels[i], prediction_probabilities[i]) for i in sorted_indices]
+    probabilities = prediction[0]
 
-    return sorted_labels
+    sorted_predictions = sorted(
+        zip(class_labels, probabilities),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    return sorted_predictions
