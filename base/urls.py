b5f7b2adbabb05e983f0b519c610b65893b331b0
ridwaanhall <erbyl14@gmail.com>
2024-06-18 23:18:26 +0700

    updated code

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('predict/', views.predict_view, name='predictZeronineEN'),
+    path('', views.zeronine_en, name='ZeronineEN'),
+    path('predict/', views.predict_view, name='PredictZeronineEN'),
 ]
