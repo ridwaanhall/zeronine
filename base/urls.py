d45446ec6004380c12590b5d8fd5abd778b92d01
ridwaanhall <erbyl14@gmail.com>
2024-06-19 07:24:58 +0700

    add redirect to en and add ar

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.zeronine_en, name='ZeronineEN'),
-    path('predict/', views.predict_view, name='PredictZeronineEN'),
+    path('', views.redirect_to_en, name='redirect_to_en'),
+    
+    path('en/', views.zeronine_en, name='ZeronineEN'),
+    path('en/predict/', views.predict_zeronine_en, name='PredictZeronineEN'),
+    
+    path('ar/', views.zeronine_ar, name='ZeronineAR'),
+    path('ar/predict/', views.predict_zeronine_ar, name='PredictZeronineAR'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from PIL import Image
 import base64
@@ -6,10 +6,17 @@ import io
 import json
 from .model import predict
 
+
+def redirect_to_en(request):
+    return redirect('ZeronineEN')
+
 def zeronine_en(request):
     return render(request, 'base/zeronine-en.html')
 
-def predict_view(request):
+def zeronine_ar(request):
+    return render(request, 'base/zeronine-ar.html')
+
+def predict_zeronine_en(request):
     if request.method == 'POST':
         try:
             if 'image' in request.FILES:
@@ -33,7 +40,36 @@ def predict_view(request):
 
             return JsonResponse({'sorted_predictions': sorted_predictions})
         except Exception as e:
-            print(f"Error in predict_view_test: {e}")
+            print(f"Error in predict_zeronine_en: {e}")
+            return JsonResponse({'error': str(e)}, status=500)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+def predict_zeronine_ar(request):
+    if request.method == 'POST':
+        try:
+            if 'image' in request.FILES:
+                uploaded_file = request.FILES['image']
+                image_content = uploaded_file.read()
+                image_file = io.BytesIO(image_content)
+                img = Image.open(image_file)
+            else:
+                data = json.loads(request.body.decode('utf-8'))
+                image_data = data.get('image').split(',')[1]
+                image_content = base64.b64decode(image_data)
+                image_file = io.BytesIO(image_content)
+                img = Image.open(image_file)
+
+            sorted_predictions = predict(img)
+
+            if not sorted_predictions:
+                return JsonResponse({'error': 'Prediction failed'}, status=500)
+
+            sorted_predictions = [(label, float(probability)) for label, probability in sorted_predictions]
+
+            return JsonResponse({'sorted_predictions': sorted_predictions})
+        except Exception as e:
+            print(f"Error in predict_zeronine_ar: {e}")
             return JsonResponse({'error': str(e)}, status=500)
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
