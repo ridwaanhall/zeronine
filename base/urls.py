82684d333f6251ca713d0d581267d8ed403e4474
ridwaanhall <erbyl14@gmail.com>
2024-07-29 22:58:11 +0700

    update url and add ar char

--- base/urls.py
+++ base/urls.py
@@ -4,9 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.redirect_to_en, name='redirect_to_en'),
     
-    path('en/', views.zeronine_en, name='ZeronineEN'),
-    path('en/predict/', views.predict_zeronine_en, name='PredictZeronineEN'),
+    path('en/number', views.zeronine_en, name='ZeronineEN'),
+    path('en/predict-number', views.predict_zeronine_en_num, name='PredictZeronineEN'),
     
-    path('ar/', views.zeronine_ar, name='ZeronineAR'),
-    path('ar/predict/', views.predict_zeronine_ar, name='PredictZeronineAR'),
+    path('ar/number', views.zeronine_ar, name='ZeronineAR'),
+    path('ar/predict-number', views.predict_zeronine_ar_num, name='PredictZeronineAR'),
+    
+    path('ar/char', views.zeronine_ar_char, name='ZeronineARChar'),
+    path('ar/predict-char', views.predict_zeronine_ar_char, name='PredictZeronineARChar'),
 ]
